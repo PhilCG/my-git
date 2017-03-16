c2e783ee06c8872e38fecea4909c35b32b26510e
phlime <philcg6@gmail.com>
2017-03-16 16:35:40 +0800

    45

--- 未命名文件夹/Cien.js
+++ 未命名文件夹/Cien.js
@@ -1,11 +1,57 @@
 //import "node_modules/babel-polyfill";
-(function() {
+var Cien = function(input, output, method, environment) {
 	"use strict";
-	var Cien = function(a){
-		var _a = a;
-		document.getElementsByClassName(_a);
-	};
-	var cien = new Cien;
-	console.log(Cien);
-	return Cien;
-}).call(Cien);
+	this.input = document.getElementsByClassName(input);
+	this.output = output;
+	this.method = method;
+	this.environment = environment;
+	//here register the goal of this func.
+}
+Cien.prototype = {
+	constructor: Cien,
+	// el: function(){
+	// 	input = document.getElementsByClassName(this.input);
+	// 	return this
+	// },
+	log: function(){
+		console.log(
+			'input = ' + this.input +
+			'; output = ' + this.output +
+			'; method = ' + this.method +
+			'; environment = ' + this.environment
+		)
+		return this//for chain method
+	}
+}
+// var z = function() {
+//     var x = 0;
+//     return { 
+//     	increment: function(inc) {
+//     		x += typeof inc === 'number' ? inc : 1; 
+//     	}, 
+//     	getX: function() {
+//         	return x;
+//         }
+// 	}
+// }()
+
+// (function(global){
+//     var myfun1=function(){
+//         console.log('myfun1');
+//     };
+//     var myfun2=function(){
+//         console.log('myfun2');
+//     };
+//     //设置你的命名空间
+//     var mypackageName="com.mydomain.utils";
+//     var packageArray=mypackageName.split(".");
+//     var finalObj=packageArray.reduce(function(prev,current){
+//         return prev[current]||(prev[current]={});
+//     }, global);
+    
+//     //将你的函数绑定到命名空间上
+//     finalObj.myfun1= myfun1;
+//     finalObj.myfun2= myfun2;
+    
+    
+// }(window));
